f2e65eacd078ebf4353eca322d06500b703be692
White Hare <whitehare.wav@gmail.com>
2024-10-22 18:34:38 +0200

    Agregar archivos de proyecto.

---
+++ Angelmindesu/Program.cs
@@ -0,0 +1,16 @@
+﻿var point = 0;
+
+Console.WriteLine("Hola galero, tienes que llegar a 100 puntos");
+while (point < 100)
+{
+    Console.ReadKey(true);
+    point++;
+    Console.WriteLine("Tienes " + point + " puntos");
+    if (point == 69)
+    {
+        Console.WriteLine("Nice");
+    }
+}
+
+Console.WriteLine("Has ganado friki. Presiona cualquier tecla para morir");
+Console.ReadKey();
