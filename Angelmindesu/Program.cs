19df678c0cc1f425724c976ac27b61f5918a86c3
White Hare <whitehare.wav@gmail.com>
2024-10-22 20:48:10 +0200

    Loops added

--- Angelmindesu/Program.cs
+++ Angelmindesu/Program.cs
@@ -1,4 +1,5 @@
-﻿var point = 0;
+﻿float point = 0.00f;
+float multiplier = 0.01f;
 
 Console.WriteLine("Hola galero, tienes que llegar a 100 puntos");
 while (point < 100)
@@ -13,4 +14,34 @@ while (point < 100)
 }
 
 Console.WriteLine("Has ganado friki. Presiona cualquier tecla para morir");
-Console.ReadKey();
+Console.ReadKey(true);
+
+point = 0;
+
+while (multiplier < 1)
+{
+    Console.WriteLine("SIKE! Vuelves a empezar (sumas " +  " por punto ahora)");
+    while (point < 100)
+    {
+        Console.ReadKey(true);
+        point += 1 + multiplier;
+        Console.WriteLine("Tienes " + point.ToString("F2") + " puntos");
+        if (Math.Abs(point - 69.69f) < 0.01f)
+        {
+            Console.WriteLine(@" /$$   /$$ /$$                    
+| $$$ | $$|__/                    
+| $$$$| $$ /$$  /$$$$$$$  /$$$$$$ 
+| $$ $$ $$| $$ /$$_____/ /$$__  $$
+| $$  $$$$| $$| $$      | $$$$$$$$
+| $$\  $$$| $$| $$      | $$_____/
+| $$ \  $$| $$|  $$$$$$$|  $$$$$$$f
+|__/  \__/|__/ \_______/ \_______/");
+        }
+    }
+    point = 0;
+    multiplier = multiplier * 2;
+}
+
+
+Console.WriteLine("Ahora si. Presiona cualquier tecla frikiton");
+Console.ReadKey(true);
